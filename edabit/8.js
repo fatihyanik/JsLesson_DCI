2743a8bc7f5b1466e85f0c57ae4ebb1c27aa76b9
fatihyanik <fatihyanik07@gmail.com>
2021-10-12 18:08:44 +0200

    i added 8.js

---
+++ edabit/8.js
@@ -0,0 +1,17 @@
+/*
+Pick a topic for research and go to the corresponding breakout room:
+Big-O notation (Room 1)
+clones and references (Room 2)
+call stack and execution contexts (Room 3)
+“No Time To Die” (Room 4)
+topic of your choice (some room number > 4)
+*/
+
+/*
+what is Big-O notation in javascript
+Big O Notation — Time Complexity in Javascript. We use a shorthand mathematical notation to describe the efficiency of an algorithm is Big O Notation, The letter O is used because the rate of growth of a function is also called its order*/
+
+
+
+/*
+A call stack is a data structure used by the JavaScript engine to manage execution contexts. The call stack uses a principle known as the LIFO (last-in-first-out) principle and whatever is at the top of the call stack will get executed first, then popped off the stack, until the stack is empty.*/
